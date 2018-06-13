f08e06ada1d4b5dc97443a0345f4999d852fd8dc
Peter N K <pnk@avb.bg>
2018-06-13 11:24:25 +0300

    Ready for Use. Created in version 2.9.14 I have to check why the prompt text has a problem and says that there is a difference between actual and hardcoded

---
+++ StepEditText/StepEditText/UtilityFunctions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StepEditText
+{
+    class RunReport
+    {
+        public static void PositiveResult(string Actual)
+        {
+            Ranorex.Report.Success("Sucessfully Perforemed", Actual);
+        }
+
+        public static void NegativeResult(string Actual, string Expected)
+        {
+            Ranorex.Report.Error("STOP THE VERSION", Actual);
+            Ranorex.Report.Error("THE EXPECTED TEXT MUST BE", Expected);
+        }
+    }
+    class UtilityRun
+    {
+        public static void InspectText(string Actual, string Hardcoded)
+        {
+            string strActualText = Actual;
+            string strExpectedText = Hardcoded;
+
+            bool bCompareText = true;
+
+            bCompareText = strActualText.CompareTo(strExpectedText) == 0;
+
+            if (bCompareText)
+                RunReport.PositiveResult(strActualText);
+
+            else
+                RunReport.NegativeResult(strActualText, strExpectedText);
+
+
+                
+        }
+    }
+}
